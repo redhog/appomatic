d51ca695bb46fa4fcc207aaa72544d44c57521cd
Egil Moeller <egil.moller@freecode.no>
2011-12-20 17:49:31 +0100

    Bugfix for not depending on multilingual

--- appomatic_django_cms/appomatic_django_cms/cms/__settings__.py
+++ appomatic_django_cms/appomatic_django_cms/cms/__settings__.py
@@ -3,3 +3,6 @@ CMS_TEMPLATES = (
 )+ get_app_config_list('CMS_TEMPLATES')
 CMS_APPLICATIONS_URLS = get_app_config_list('CMS_APPLICATIONS_URLS')
 CMS_NAVIGATION_EXTENDERS = get_app_config_list('CMS_NAVIGATION_EXTENDERS')
+
+LANGUAGES = [('en', 'English')] # Override this if you're using multilingual
+DEFAULT_LANGUAGE = 0
