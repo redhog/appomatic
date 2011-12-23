5eb40cbffc5e79acfeace8dd4c05c18ad24109af
Egil Moeller <egil.moller@freecode.no>
2011-12-23 11:45:27 +0100

    Bugfix for order of installation

--- appomatic_cms_tagging/appomatic_cms_tagging/migrations/0001_add_tag_field.py
+++ appomatic_cms_tagging/appomatic_cms_tagging/migrations/0001_add_tag_field.py
@@ -4,7 +4,7 @@ import tagging.fields
 
 class Migration:    
     def forwards(self, orm):
-        db.add_column('cms_page', 'tag_field', tagging.fields.TagField())
+        db.add_column('cms_page', 'tag_field', tagging.fields.TagField(default=''))
         
     def backwards(self, orm):
         db.delete_column('cms_page', 'tag_field')
