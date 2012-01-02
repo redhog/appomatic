6b8755d762ea7de3f6e8ff8ec547259d7a1eb829
Nikola Trandafilovic <nt@freecode.no>
2012-01-03 00:47:31 +0100

    Added url for application details

--- appomatic_appadmin/appomatic_appadmin/__urls__.py
+++ appomatic_appadmin/appomatic_appadmin/__urls__.py
@@ -4,4 +4,5 @@ from django.conf import settings
 urlpatterns = patterns('',
     (r'^admin/appomatic_appadmin/application/?$', 'appomatic_appadmin.views.index'),
     (r'^admin/appomatic_appadmin/application/add/?$', 'appomatic_appadmin.views.add'),
+    (r'^admin/appomatic_appadmin/application/(?P<app_name>\w+)/$', 'appomatic_appadmin.views.details'),
 )
