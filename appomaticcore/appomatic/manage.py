cc5b824695a1a37aa878991d9e2d9e02cf8c9312
root <root@ew4.(none)>
2013-02-19 15:42:47 +0000

    Support for newer django version

--- appomaticcore/appomatic/manage.py
+++ appomaticcore/appomatic/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
+
+import django
 import os.path
 import sys
 
@@ -9,8 +10,15 @@ if sys.argv[1] not in ('makemessages', 'compilemessages'):
 import settings
 
 def main():
-    execute_manager(settings)
+
+    if django.VERSION[0] >= 1 and django.VERSION[1] >= 5:
+
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appomatic.settings")
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
+    else:
+        from django.core.management import execute_manager
+        execute_manager(settings)
 
 if __name__ == "__main__":
     main()
-
